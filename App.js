fcc4783a1eb8c2efba0ddbd808ba9237c19ccc44
gatiistvan23 <gatiistvan@ktch.hu>
2024-12-12 11:11:18 +0100

    Stile kész

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-web';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView style={styles.container}>
+      <view style={styles.header}>
+        <Text style={styles.headerText}>
+          Fejrész
+        </Text>
+      </view>
+      <View style={styles.body}>
+        <Text style={styles.bodyText}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, deleniti? Cum accusamus, impedit error placeat eum rerum quia, odio assumenda molestiae aliquam dignissimos vitae sit alias perferendis iure id voluptatibus!
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, deleniti? Cum accusamus, impedit error placeat eum rerum quia, odio assumenda molestiae aliquam dignissimos vitae sit alias perferendis iure id voluptatibus!
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, deleniti? Cum accusamus, impedit error placeat eum rerum quia, odio assumenda molestiae aliquam dignissimos vitae sit alias perferendis iure id voluptatibus!        
+        </Text>
+      </View>
+      <view style={styles.footer}>
+        <Text>Lábrész</Text>
+      </view>
+
       <StatusBar style="auto" />
-    </View>
+      
+    </ScrollView>    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#0f0',
+    // alignItems: 'center',
+    // justifyContent: 'center',    
   },
+  header: {
+    marginTop: 24,    
+    backgroundColor: 'red',
+    width: '100%',
+    padding: 10,
+    textAlign: 'center',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 40,    
+  },
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center', 
+  },
+  bodyText: {
+    fontSize: 20,
+  },
+  footer: {},
 });
